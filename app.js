c39d6d0f30ff80fbef8a3adc763cb07b517e1f47
Perion <nsehnoun@student.42.fr>
2018-10-01 13:34:24 +0200

    Added quantity check

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/amits_workshop");
+mongoose.connect("mongodb://localhost/amits_workshop", { useNewUrlParser: true });
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-//Creating ingredients model so Chief Amit can find his stuff easier.
+// Creating ingredients model so Chief Amit can find his stuff easier.
 var ingredientModel = new mongoose.Schema({
     name: String,
     quantity: Number,
     unit: String
 });
 var Ingredient = mongoose.model("Ingredient", ingredientModel);
-
 var recipe_file_index = 0;
+
 app.get("/", function (request, response) {
-    //Displaying ingredients stored in DB
+    // Displaying ingredients stored in DB
     Ingredient.find({}, function (error, ingredient) {
         if (error)
             console.log(error);
@@ -26,46 +26,51 @@ app.get("/", function (request, response) {
             response.render("index", { elements: ingredient });
     });
 });
-app.get("/recipes", function (request, response) {
-    response.render("recipes");
-});
+
+// Fetch recipes from file in /json 
+// and check their availability comparing the ingredients needed to those in DB.
 app.get("/recipes/:ele", function (request, response) {
     if (request.query.recipe) {
-        if (recipe_file_index == request.query.recipes.length)
-            recipe_file_index = 0;
         var recipe_ingredients = request.query.recipe.ingredients;
         var obj = Object.keys(recipe_ingredients);
-        var obj_len = obj.length;
+        obj.forEach(function (o) {
+            if (recipe_ingredients[o] === 'true')
+                recipe_ingredients[o] = 0;
+        });
         var available_ingredients = [];
-        var recipe_file;
-        var recipe_name;
+
+        if (recipe_file_index == request.query.recipes.length)
+            recipe_file_index = 0;
         Ingredient.find({}, function (error, ingredients) {
             if (error)
                 console.log(error);
             else {
                 ingredients.forEach(function (name) {
-                    obj.forEach(function (ingredient_name) {
-                        if (ingredient_name.localeCompare(name.name) == 0)
-                            available_ingredients.push(ingredient_name);
+                    obj.forEach(function (ingredient_name, i) {
+                        if (ingredient_name.localeCompare(name.name) == 0
+                            && (name.quantity >= recipe_ingredients[ingredient_name]))
+                            available_ingredients.push({
+                                i_name: ingredient_name,
+                                q: recipe_ingredients[ingredient_name],
+                                index: i
+                            });
                     });
                 });
                 if (obj.length == available_ingredients.length) {
-                    request.query.recipe["recipe_file"] = request.query.recipes[recipe_file_index++];                    
-                    recipe_file = request.query.file_name;
-                    recipe_name = request.query.recipe.name;
+                    request.query.recipe["recipe_file"] = request.query.recipes[recipe_file_index++];
+                    console.log(available_ingredients[0].i_name);
                     response.send({
                         ingredients: available_ingredients,
-                        recipe_name: recipe_name,
-                        recipe_file: recipe_file
+                        recipe_name: request.query.recipe.name,
+                        recipe_file: request.query.file_name
                     });
-                    console.log("You have the ingredients to make " + request.query.recipe.name + "!");
                 }
             }
         });
     }
 });
 
-//Update && add in one function
+// Update && add in one function
 app.post("/addElement/:manage", function (request, response) {
     var id = request.body.ingredient_id;
     var element = request.body.ingredient_name;
@@ -76,7 +81,7 @@ app.post("/addElement/:manage", function (request, response) {
 
     i = id;
     if (request.params.manage.localeCompare("update") == 0) {
-        //Updating existing ingredients
+        // Updating existing ingredients
         Ingredient.findByIdAndUpdate(i, new_element, function (error, ingredient) {
             if (error) {
                 console.log("Couldn't update ingredient.");
@@ -89,7 +94,7 @@ app.post("/addElement/:manage", function (request, response) {
         });
     }
     else {
-        //Creating new ingredients
+        // Creating new ingredients
         Ingredient.create(new_element, function (error, ingredient) {
             if (error) {
                 console.log("Couldn't save new ingredient.");
@@ -104,6 +109,7 @@ app.post("/addElement/:manage", function (request, response) {
     response.redirect("/");
 });
 
+// Delete an ingredient
 app.get("/deleteElement", function (request, response) {
     var i;
     var index;
@@ -123,8 +129,8 @@ app.get("/deleteElement", function (request, response) {
     response.redirect("/");
 });
 
-//Function to manage adding or reducing quantity.
-//the HTML classes are respectively and elegantly named : plus_button and minus_button
+// Function to manage adding or reducing quantity.
+// the HTML classes are respectively and elegantly named : plus_button and minus_button
 app.get("/quantity/:manage/:quantity", function (request, response) {
     var i;
     var index;
